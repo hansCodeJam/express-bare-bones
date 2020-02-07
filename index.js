78c50cc9c3e6035a6d128aef00fdbb6d286b1fc2
Hans Vos <hans.vos@codeimmersives.com>
2020-02-07 15:10:49 -0500

    added routes folder and userRoutes.js

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const express = require('express');
 const app = exrpess();
 const logger = require('morgan');
 const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/users', userRoutes)
+
+app.get('/', (req, res) => {
+    return res.send('Hello express')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+}
+)

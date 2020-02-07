e309652cf09d73f15ad49c926a9a34f3262c0bcc
Hans Vos <hans.vos@codeimmersives.com>
2020-02-07 16:50:16 -0500

    add postman json file

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,5 +29,34 @@ module.exports = {
         users.push(newUser);
         console.log(newUser)
         return res.json(req.body);
+    }, 
+
+    updatedUser: (req, res) => {
+        const userExists = users.filter(user => user.id === req.params.id);
+        const user = userExists[0]
+        if(user.id !== 0) {
+            const updatedUser = req.body;
+            if(user.id === req.params.id) {
+                user.name = updatedUser.name ? updatedUser.name : user.name;
+                user.email = updatedUser.email ? updatedUser.email : user.email;
+                res.status(200).json({message:'User updated', user });
+            }
+        } else {
+            return res.status(400).json( {message: `User with id: ${req.params.id} does not exist`} );
+        }
+    }, 
+
+    deleteUser: (req, res)=> {
+        const userExists = users.filter(user => user.id === req.params.id);
+        let user = userExists[0];
+    
+        if(userExists.length !== 0) {
+            const deletedUser = users.indexOf(user);
+            users.splice(deletedUser, 1);
+            return res.status(200).json({message: 'User Deleted', user })
+    
+        } else {
+            return res.status(400).json({message: `User with id: ${req.params.id} does not exists`})
+        }
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,8 @@ const userController = require('../controllers/userController')
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getSingleUser);
 router.post('/', userController.createUser);
+router.put('/:id', userController.updatedUser);
+router.delete('/:id', userController.deleteUser);
+
 
 module.exports = router;
